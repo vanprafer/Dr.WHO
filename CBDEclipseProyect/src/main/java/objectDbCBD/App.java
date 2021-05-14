ac2bcac982a347e59936d208f71792f40d941780
Alegomcab1 <alegomcab1@gmail.com>
2021-05-14 14:59:05 +0200

    Updated the import

--- CBDEclipseProyect/src/main/java/objectDbCBD/App.java
+++ CBDEclipseProyect/src/main/java/objectDbCBD/App.java
@@ -2,6 +2,7 @@ package objectDbCBD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class App {
