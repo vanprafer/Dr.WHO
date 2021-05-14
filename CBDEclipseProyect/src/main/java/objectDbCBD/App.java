07627a25b4f7067b325953b0cbe82d5692482114
Alegomcab1 <alegomcab1@gmail.com>
2021-05-14 14:57:59 +0200

    Update annotation for application

--- CBDEclipseProyect/src/main/java/objectDbCBD/App.java
+++ CBDEclipseProyect/src/main/java/objectDbCBD/App.java
@@ -3,7 +3,7 @@ package objectDbCBD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class App {
   public static void main(String[] args) {
         SpringApplication.run(App.class, args);
